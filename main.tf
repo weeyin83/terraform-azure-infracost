c34d50116b65b45d21385623233fa07dbf2cbe3e
Sarah Lean <sarahlean@microsoft.com>
2025-12-18 15:42:39 +0000

    add a storage account

--- main.tf
+++ main.tf
@@ -64,3 +64,11 @@ resource "azurerm_service_plan" "appplan" {
   os_type             = "Linux"
   sku_name            = "B1"
 }
+
+resource "azurerm_storage_account" "storage" {
+  name                     = module.naming.storage_account.name_unique
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
